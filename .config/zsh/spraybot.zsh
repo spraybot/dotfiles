88cf9bdf1bc7a14038c7944219ea0e3e0ebc6117
Shrijit Singh <shrijitsingh99@gmail.com>
2022-09-01 09:44:51 -0400

    Split environment vars into seperate file

--- .config/zsh/spraybot.zsh
+++ .config/zsh/spraybot.zsh
@@ -1,23 +1,7 @@
 # Device specific zshrc file (Not to be tracked on git)
 
-# FastDDS Config
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-
-if [[ "${USER}" == "spraybot" ]]; then
-    export CYCLONEDDS_URI="/home/${USER}/.config/dds/spraybot.xml"
-else
-    export CYCLONEDDS_URI="/home/${USER}/.config/dds/dev.xml"
-fi
-
-# ROS1 Husky Config
-export HUSKY_LOGITECH=1
-export HUSKY_JOY_DEVICE="/dev/input/f710"
-
-# base_link -> top_plate_rear_link
-# Translation: [-0.331, 0.000, 0.232]
-export HUSKY_IMU_XYZ="0.112 0.0 0.458"
-export HUSKY_IMU_RPY="-3.14 0.0 0.0"
-export HUSKY_LARGE_TOP_PLATE="true"
+# Source environment variables
+source ${ZDOTDIR:-$HOME}/spraybot_env
 
 # Spraybot
 export SPRAYBOT_IP="192.168.0.5"
@@ -29,6 +13,3 @@ alias ssh-spraybot="ssh -X spraybot@${SPRAYBOT_IP}"
 alias spraybot2local="rsync -r -t -v --progress -u -z -b -s spraybot@${SPRAYBOT_IP}:/home/spraybot/spraybot_ws/src/ /home/${USER}/remote_spraybot_ws/src/"
 # local2spraybot
 alias local2spraybot="rsync -r -t -v --progress -u -z -b -s /home/${USER}/remote_spraybot_ws/src/ spraybot@${SPRAYBOT_IP}:/home/spraybot/spraybot_ws/src/"
-
-export ROS_DOMAIN_ID=0
-export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
