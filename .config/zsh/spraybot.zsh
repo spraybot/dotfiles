ddbbbe6c9063f8c920f997abb3c97e78bc221495
Shrijit Singh <shrijitsingh99@gmail.com>
2022-09-06 13:00:27 -0400

    Add zenoh dds bridge

--- .config/zsh/spraybot.zsh
+++ .config/zsh/spraybot.zsh
@@ -13,3 +13,5 @@ alias ssh-spraybot="ssh -X spraybot@${SPRAYBOT_IP}"
 alias spraybot2local="rsync -r -t -v --progress -u -z -b -s spraybot@${SPRAYBOT_IP}:/home/spraybot/spraybot_ws/src/ /home/${USER}/remote_spraybot_ws/src/"
 # local2spraybot
 alias local2spraybot="rsync -r -t -v --progress -u -z -b -s /home/${USER}/remote_spraybot_ws/src/ spraybot@${SPRAYBOT_IP}:/home/spraybot/spraybot_ws/src/"
+
+alias zenoh="zenoh-bridge-dds -d ${ROS_DOMAIN_ID} --fwd-discovery"
